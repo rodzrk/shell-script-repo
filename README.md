6879cb1940df72d193c70a95cb0421e98408e22a
rodzrk <rod_work@126.com>
2019-08-04 18:25:08 +0800

    提交重置yum源的脚本

--- README.md
+++ README.md
@@ -8,3 +8,7 @@ This repository is mainly used to store some commonly used scripts.
     - This program is used to create, start, stop or clear the redis-cluster.
     - But the redis version need to be greater than 5;
     - Now the script only supports creating clusters on one node.
+    
+## II. linux
+
+- 
--- linux/reset-yum-config.sh
+++ linux/reset-yum-config.sh
@@ -1,1 +1,69 @@
-#!/usr/bin/env bash
+#!/bin/bash
+# Program:
+# This program is used to reset the yum config.
+#
+# History:
+# 2019/08/04 rod First release
+#
+# Prepared:
+#
+# Note:
+#
+
+
+# 备份原文件
+mv -f /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup
+
+# 执行写入操作
+cat > /etc/yum.repos.d/CentOS-Base.repo << EOF
+# CentOS-Base.repo
+#
+# The mirror system uses the connecting IP address of the client and the
+# update status of each mirror to pick mirrors that are updated to and
+# geographically close to the client.  You should use this for CentOS updates
+# unless you are manually picking other mirrors.
+#
+# If the mirrorlist= does not work for you, as a fall back you can try the
+# remarked out baseurl= line instead.
+#
+#
+
+[base]
+name=CentOS-$releasever - Base
+#mirrorlist=http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=os
+baseurl=https://mirrors.ustc.edu.cn/centos/$releasever/os/$basearch/
+gpgcheck=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
+
+#released updates
+[updates]
+name=CentOS-$releasever - Updates
+# mirrorlist=http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=updates
+baseurl=https://mirrors.ustc.edu.cn/centos/$releasever/updates/$basearch/
+gpgcheck=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
+
+#additional packages that may be useful
+[extras]
+name=CentOS-$releasever - Extras
+# mirrorlist=http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=extras
+baseurl=https://mirrors.ustc.edu.cn/centos/$releasever/extras/$basearch/
+gpgcheck=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
+
+#additional packages that extend functionality of existing packages
+[centosplus]
+name=CentOS-$releasever - Plus
+# mirrorlist=http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=centosplus
+baseurl=https://mirrors.ustc.edu.cn/centos/$releasever/centosplus/$basearch/
+gpgcheck=1
+enabled=0
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
+
+EOF
+
+# 清空缓存重新配置
+yum clean all
+rm -rf /var/cache/
+rm -rf /var/cache/yum
+yum makecache
